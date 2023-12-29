abbd687e60a4cb6581843308a6ac8ea5fc06827c
Chaitanya1603 <cdwaghaye@gmail.com>
2023-12-29 16:36:54 +0530

    Few changes in the html and js files

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
 const toggler = document.querySelector(".toggler");
-const starterPrice = document.getElementById("starter-price");
-const proPrice = document.getElementById("pro-price");
+const basicPrice = document.getElementById("basic-price");
+const premiumPrice = document.getElementById("premium-price");
+const membershipDurationSelect = document.getElementById("membership-duration");
 
-toggler.addEventListener("change", ()=>{
-    if(toggler.checked){
-        starterPrice.innerHTML = `$200 <span> / year </span>`;
-        proPrice.innerHTML = `$1000 <span> / year </span>`;
-    }else{
-        starterPrice.innerHTML = `$19 <span> / month </span>`;
-        proPrice.innerHTML = `$99 <span> / month </span>`; 
+toggler.addEventListener("change", () => {
+    updatePrices();
+});
+
+membershipDurationSelect.addEventListener("change", () => {
+    updatePrices();
+});
+
+function updatePrices() {
+    const isAnnuallyBilled = toggler.checked;
+    const basicMonthlyPrice = 500;
+    const premiumMonthlyPrice = 1000;
+
+    if (isAnnuallyBilled) {
+        const selectedDuration = parseInt(membershipDurationSelect.value);
+        const basicPriceInr = basicMonthlyPrice * selectedDuration * 12;
+        const premiumPriceInr = premiumMonthlyPrice * selectedDuration * 12;
+
+        basicPrice.innerHTML = `₹${basicPriceInr} <span> / year </span>`;
+        premiumPrice.innerHTML = `₹${premiumPriceInr} <span> / year </span>`;
+    } else {
+        const selectedDuration = parseInt(membershipDurationSelect.value);
+        const basicPriceInr = basicMonthlyPrice * selectedDuration;
+        const premiumPriceInr = premiumMonthlyPrice * selectedDuration;
+
+        basicPrice.innerHTML = `₹${basicPriceInr} <span> / month </span>`;
+        premiumPrice.innerHTML = `₹${premiumPriceInr} <span> / month </span>`;
     }
-})
+}
+
+// Initial update
+updatePrices();
